87dc2ec69a6e69dc4f50ac855e3418c9b89bfbc0
jshong0907 <jshong0907@naver.com>
2018-07-25 04:39:45 +0900

    ADD 회원관리

--- apis/Database_Table.php
+++ apis/Database_Table.php
@@ -9,15 +9,18 @@ $sql="SELECT * FROM user";
 echo "<script>var inner_html='';</script>";
 if ($result=mysqli_query($db,$sql))
 {
+  //echo "<script>var i = 0;</script>";
   while ($row=mysqli_fetch_row($result))
   {
     $id = $row[0];
     $pw = $row[1];
     $email = $row[2];
     $phone = $row[3];
-
-    echo "<script>inner_html += '<tr><td>$id</td><td>$pw</td><td>$email</td><td>$phone</td></tr>'; </script>";
+    $allowed = $row[4];
+    //echo "<script>var n = i.toString();</script>";
+    echo "<script>inner_html += '<tr><td name=\'id\'>$id</td><td>$pw</td><td>$email</td><td>$phone</td><td>$allowed</td><td><form method=\'POST\' action=\"./apis/allow_register.php\"><button name=\'btn\' value=$id>승인</button></form><form method=\'POST\' action=\"./apis/refuse_register.php\"><button name=\'btn\' value=$id>취소</button></form></td><td><form method=\'POST\' action=\"./apis/delete_register.php\"><button name=\'btn\' value=$id>삭제</button></form></td></tr>'; </script>";
     //echo "<script>document.getElementById('table').innerHTML='<tr><td>$id</td><td>$pw</td><td>$email</td><td>$phone</td></tr>';</script>";
+    //echo "<script>i += 1;</script>";
   }
   echo "<script>document.getElementById('table').innerHTML = inner_html;</script>";
   mysqli_free_result($result);
@@ -35,4 +38,10 @@ while ($row=mysqli_fetch_row($result))
   echo "<script>document.getElementById('table').'innerHTML=<tr><td>$id</td><td>$pw</td><td>$email</td><td>$phone</td></tr>';</script>";
 }*/
 mysqli_close($db);
+
+/*function allow()
+{
+  echo "<script>$update_id = document.getElementById("$i").innerHTML;</script>";
+  $sql = "UPDATE `user` SET `allowed`=true WHERE ID=$update_id";
+}*/
 ?>
---
+++ apis/delete_register.php
@@ -0,0 +1,21 @@
+<?php
+
+$id=$_POST["btn"];
+$db = mysqli_connect('127.0.0.1', 'root', 'autoset', 'dasom');
+if(mysqli_connect_errno())
+{
+  echo "연결에러";
+}
+$sql = "DELETE FROM `user` WHERE ID='$id'";
+echo "$sql";
+if($result = mysqli_query($db, $sql))
+{
+  mysqli_query($db, $sql);
+  mysqli_close($db);
+  echo "<script>window.location.replace('../Mainpage.html');</script>";
+}
+else {
+  echo "good";
+}
+
+?>
